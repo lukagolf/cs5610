e0ccd03aba587b07c7652fc828b3704d7b259051
lukagolf <jovanovic.l@northeastern.edu>
2023-06-08 16:13:50 -0400

    Trying to establish a remote connection over Heroku

--- src/tuiter/services/tuits-service.js
+++ src/tuiter/services/tuits-service.js
@@ -1,15 +1,17 @@
 import axios from 'axios';
 
 const API_BASE = process.env.REACT_APP_API_BASE;
+/* const API_BASE = "https://lj-tuiter-node-server-app.herokuapp.com/api";
+   const API_BASE = "http://localhost:4000/api" */
 const TUITS_API = `${API_BASE}/tuits`;
 
 export const createTuit = async (tuit) => {
+    console.log(TUITS_API);
     console.log("inside tuit service", tuit)
     const response = await axios.post(TUITS_API, tuit)
     console.log("inside tuit service from backend", response)
     return response.data;
 }
-
 
 export const findTuits = async () => {
     const response = await axios.get(TUITS_API);
