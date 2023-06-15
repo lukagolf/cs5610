7aa48a685471230c72ae1eb41c465ebbd5cb35c6
lukagolf <jovanovic.l@northeastern.edu>
2023-06-15 13:31:05 -0400

    Ready for Netlify, with global variable

--- src/tuiter/services/tuits-service.js
+++ src/tuiter/services/tuits-service.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE = process.env.REACT_APP_SERVER_API_URL;
+const API_BASE = process.env.REACT_APP_API_BASE;
 const TUITS_API = `${API_BASE}/tuits`;
 // const API_BASE = "https://lj-tuiter-node-server-app.herokuapp.com/api";
 // const TUITS_API = 'http://localhost:4000/api/tuits';
